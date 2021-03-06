5e7516343297035d419d9ef17d20978165ba0e06
CHAIKOU AMADOU BALDE <chaikou-amadou.balde@atos.net>
2021-03-06 13:44:20 +0000

    Corrected pom h2 DB and REMOTE_HOST for deployment

--- src/main/java/net/hub4u/ebgsys/entities/Sale.java
+++ src/main/java/net/hub4u/ebgsys/entities/Sale.java
@@ -12,7 +12,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
@@ -20,7 +19,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Data
